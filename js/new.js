0a89f6b3b88d4e37d995d6f1bc5d22f45b342f19
wisdomI <81252474+wisdomI@users.noreply.github.com>
2021-12-02 10:25:04 +0100

    update

--- js/new.js
+++ js/new.js
@@ -1,39 +1,82 @@
-let black = [
+let discount = [
     {
-        name: 'MERCII',
-        model: '2004',
-        image: './images/car3.jfif',
-        price: '$300,000'
+        cName: 'Triple Jay',
+        cModel: 'Futuristic 2095 edition',
+        cImage: './images/car4.jfif',
+        cPrice: '$4,000'
     },
 
     {
-        name: 'MCRONNI',
-        model: '2004',
-        image: './images/car4.jfif',
-        price: '$300,000'
-    }
-]
-console.log(black)
+        cName: 'Quad bay',
+        cModel: 'silly seal',
+        cImage: './images/car3.jfif',
+        cPrice: '$70,000'
+    },
 
-let moto2 = document.getElementById('gallery02');
+    {
+        cName: 'SkyWalker',
+        cModel: 'SymbolizeG3',
+        cImage: './images/Aston Martin V12 Vanquish (2001) Silhouette.png',
+        cPrice: '$25,000'
+    },
+
+    {
+        cName: 'MortalMan Rider',
+        cModel: 'Escape L6',
+        cImage: './images/incredible Maserati Levante Trofeo.jfif',
+        cPrice: '$85,000'
+    },
+
+    {
+        cName: 'Deal Seal',
+        cModel: 'SONTA',
+        cImage: './images/car4.jfif',
+        cPrice: '$40,000'
+    },
+
+    {
+        cName: 'SpaceNavigator 30SF',
+        cModel: 'Bull limited edition',
+        cImage: './images/car3.jfif',
+        cPrice: '$58,999'
+    },
+
+    {
+        cName: 'Walking Thanos',
+        cModel: 'The Fifth Stone',
+        cImage: './images/Aston Martin V12 Vanquish (2001) Silhouette.png',
+        cPrice: '$250,000'
+    },
+
+    {
+        cName: 'Poseidons chariot',
+        cModel: 'chariot of wind L6',
+        cImage: './images/incredible Maserati Levante Trofeo.jfif',
+        cPrice: '$285,000'
+    }
+    
+]
+console.log(discount)
+
+let discountgallery = document.getElementById('discountgallery');
 let x = "";
-cars.forEach(f => {
+discount.forEach(e => {
     x += `
-                <div class="card mb-3 mx-2 col-md-6 col-lg-6 col-sm-12" style="max-width: 540px;">
-                <div class="row g-0">
-                <div class="col-md-4">
-                    <img src="${f.image}" class="img-fluid rounded-start" alt="...">
-                </div>
-                    <div class="col-md-8">
+                
+                <div class="row row-cols-1 row-cols-md-2 g-4 px-5 pt-4 d-flex justify-content-center">
+                <div class="col-md-6 col-sm-12 col-lg-6">
+                <div class="card">
+                    <img src="${e.cImage}" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">${f.name}</h5>
-                        <p class="card-text">${f.model}</p>
-                        <p class="card-text">${f.price}<small class="text-muted">Last updated 3 mins ago</small></p>
-                        <button type="button" class="btn btn-dark text-primary">Get Now</button>
+                    <h5 class="card-title">${e.cName}</h5>
+                    <p class="card-text">${e.cModel}</p>
+                    ${e.cPrice}
                     </div>
-                    </div>
+                    <button type="button" class="btn btn-primary">Buy Now</button>
                 </div>
+                </div>
+            </div>
             `
-    console.log(f)
-    moto2.innerHTML = x
+    console.log(e)
+    discountgallery.innerHTML = x
 });
